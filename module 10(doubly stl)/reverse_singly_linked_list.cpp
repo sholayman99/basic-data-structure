63bdc482c9446e42bcf89b5cee94ba744f52756c
sholayman99 <mdsholayman998@gmail.com>
2025-07-10 21:22:03 +0600

    leanred singly and doubly reverse

---
+++ module 10(doubly stl)/reverse_singly_linked_list.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    tail = newnode;
+}
+
+void reverse_linked_list(Node *&head, Node *&tail, Node *tmp)
+{
+    if (tmp->next == NULL)
+    {
+        head = tmp;
+        return;
+    }
+    reverse_linked_list(head, tail, tmp->next);
+    // code
+    tmp->next->next = tmp;
+    tmp->next = NULL;
+    tail = tmp;
+};
+
+void print_linked_list(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, val);
+    }
+
+    reverse_linked_list(head, tail, head);
+    //print_linked_list(head);
+    cout << head->val << endl;
+    return 0;
+}
