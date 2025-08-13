8c600c1e1bc545876ce5f84cf798913d8e4f325c
sholayman99 <mdsholayman998@gmail.com>
2025-08-13 20:46:55 +0600

    learned priority queue stl

---
+++ module 23(stl priority queue)/min_priority_queue.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    priority_queue<int,vector<int>,greater<int>> pq;
+
+    pq.push(10);
+    pq.push(20);
+    cout <<  pq.top() << endl;
+    pq.push(30);
+    pq.push(40);
+    cout <<  pq.top() << endl;
+    pq.pop();
+    cout <<  pq.top() << endl;
+     pq.pop();
+    cout <<  pq.top() << endl;
+
+    return 0;
+}
